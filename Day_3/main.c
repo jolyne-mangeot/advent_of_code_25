e8654cb1ce6e106d0c1b845699204f34aae97e17
Jolyne Mangeot <jmangeot@c1r5s4.marseille.42.school>
2025-12-06 18:29:49 +0100

    progress(day 3): update algorithm to recursive to adapt to puzzle's part 2

--- Day_3/main.c
+++ Day_3/main.c
@@ -6,7 +6,7 @@
 /*   By: jmangeot <jmangeot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 15:19:55 by jmangeot          #+#    #+#             */
-/*   Updated: 2025/12/06 17:00:20 by jmangeot         ###   ########.fr       */
+/*   Updated: 2025/12/06 18:27:11 by jmangeot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static int	high_value_index(char *bank, int len)
 	else
 		return (0);
 	index = 0;
-	while (bank[index] && index < len)
+	while (bank[index] && index <= len)
 	{
 		if (bank[index++] > bank[highest_value])
 			highest_value = index - 1;
@@ -32,43 +32,45 @@ static int	high_value_index(char *bank, int len)
 	return (highest_value);
 }
 
+static void	recursive_batt(char *bank, char *voltage,
+	size_t index, int batt_pos)
+{
+	size_t	bank_len;
+	size_t	bat_index;
+
+	bank_len = ft_strlen(bank) - 1;
+	bat_index = high_value_index(bank + index, bank_len - batt_pos - index)
+		+ index;
+	voltage[12 - batt_pos] = bank[bat_index];
+	if (batt_pos > 1)
+		recursive_batt(bank, voltage, bat_index + 1, batt_pos - 1);
+}
+
 int	main(void)
 {
-	size_t	count;
-	size_t	bank_len;
-	char	*bank;
-	char	voltage[3];
-	size_t	index;
-	int		fd;
+	long long int	count;
+	char			voltage[13];
+	char			*bank;
+	int				fd;
+	int				index;
 
 	fd = open("voltages", O_RDONLY);
 	if (fd < 0)
 		return (1);
-	voltage[2] = '\0';
+	ft_bzero(voltage, 12);
 	count = 0;
+	index = 1;
 	while (1)
 	{
 		bank = get_next_line(fd);
 		if (!bank)
 			break ;
-		bank_len = ft_strlen(bank) - 1;
-		index = high_value_index(bank, bank_len);
-		ft_printf("highest value : %c\n", bank[index]);
-		if (index == bank_len - 1)
-		{
-			voltage[0] = bank[high_value_index(bank, index)];
-			voltage[1] = bank[index];
-		}
-		else
-		{
-			voltage[0] = bank[index];
-			voltage[1] = bank[high_value_index(
-				bank + index + 1, bank_len - index) + index + 1];
-		}
-		ft_printf("voltage : %d\n", ft_atoi(voltage));
-		count += ft_atoi(voltage);
+		recursive_batt(bank, voltage, 0, 12);
+		printf("bank %d : %llu\n", index, ft_atoll(voltage));
+		count += ft_atoll(voltage);
+		index++;
 		free(bank);
 	}
-	printf("%lu", count);
+	printf("\n\nvoltage : %llu\n", count);
 	return (0);
 }
